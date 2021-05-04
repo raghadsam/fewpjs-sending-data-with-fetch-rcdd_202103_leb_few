9e7acd889ced5edd8d4a3d51ddcccdcc90fa1307
Raghad Al Samrout <raghadsamrout@ieee.org>
2021-05-04 22:36:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+let body=document.getElementById("body");
+let message = 'Unauthorized Access';
+let rando
+function submitData(userName,userEmail){
+const formData = {
+  name: userName,
+  email: userEmail
+};
+
+const configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify(formData)
+};
+
+return fetch("http://localhost:3000/users", configObj)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(json) {
+        body.innerHTML = json.id;
+            console.log(json);
+        })
+        .catch(function(error) {
+                 body.innerHTML = error.message;
+                 console.log(error.message);
+             });
+}
+submitData();
